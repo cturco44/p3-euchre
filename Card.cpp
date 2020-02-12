dee4aaa0232710b9b7b2f87c6a272d1f4a37dd3e
mgmii <mgmii@umich.edu>
2020-02-12 12:26:53 -0500

    Added functionality to card.cpp

--- Card.cpp
+++ Card.cpp
@@ -26,4 +26,141 @@ constexpr const char* const Card::SUIT_HEARTS;
 constexpr const char* const Card::SUIT_CLUBS;
 constexpr const char* const Card::SUIT_DIAMONDS;
 
-// add your code below
+//EFFECTS Initializes Card to the Two of Spades
+Card::Card()
+{
+    rank = RANK_TWO;
+    suit = SUIT_SPADES;
+}
+
+//EFFECTS Returns true if lhs is lower value than rhs.
+//  Does not consider trump.
+bool operator<(const Card &lhs, const Card &rhs)
+{
+    //Find suit of lhs
+    int ls = 0;
+    for(; ls < NUM_SUITS; ++ls)
+    {
+        if(lhs.get_suit() == SUIT_NAMES_BY_WEIGHT[ls])
+            break;
+    }
+
+    //Find rank of rhs
+    int rs = 0;
+    for(; rs < NUM_RANKS; ++rs)
+    {
+        if(rhs.get_rank() == RANK_NAMES_BY_WEIGHT[rs])
+            break;
+    }
+    
+    //Check suit
+    if(ls < rs)
+        return true;
+    else if(ls > rs)
+        return false;
+    
+    //Find rank of lhs
+    int lr = 0;
+    for(; lr < NUM_RANKS; ++lr)
+    {
+        if(lhs.get_rank() == RANK_NAMES_BY_WEIGHT[lr])
+            break;
+    }
+
+    //Find rank of rhs
+    int rr = 0;
+    for(; rr < NUM_RANKS; ++rr)
+    {
+        if(rhs.get_rank() == RANK_NAMES_BY_WEIGHT[rr])
+            break;
+    }
+    
+    //Check
+    if(lr < rr)
+        return true;
+    else 
+        return false;
+}
+
+//EFFECTS Returns true if lhs is higher value than rhs.
+//  Does not consider trump.
+bool operator>(const Card &lhs, const Card &rhs)
+{
+    //Find suit of lhs
+    int ls = 0;
+    for(; ls < NUM_SUITS; ++ls)
+    {
+        if(lhs.get_suit() == SUIT_NAMES_BY_WEIGHT[ls])
+            break;
+    }
+
+    //Find rank of rhs
+    int rs = 0;
+    for(; rs < NUM_RANKS; ++rs)
+    {
+        if(rhs.get_rank() == RANK_NAMES_BY_WEIGHT[rs])
+            break;
+    }
+    
+    //Check suit
+    if(ls > rs)
+        return true;
+    else if(ls < rs)
+        return false;
+    
+    //Find rank of lhs
+    int lr = 0;
+    for(; lr < NUM_RANKS; ++lr)
+    {
+        if(lhs.get_rank() == RANK_NAMES_BY_WEIGHT[lr])
+            break;
+    }
+
+    //Find rank of rhs
+    int rr = 0;
+    for(; rr < NUM_RANKS; ++rr)
+    {
+        if(rhs.get_rank() == RANK_NAMES_BY_WEIGHT[rr])
+            break;
+    }
+    
+    //Check
+    if(lr > rr)
+        return true;
+    else 
+        return false;
+}
+
+//EFFECTS Returns true if lhs is same card as rhs.
+//  Does not consider trump.
+bool operator==(const Card &lhs, const Card &rhs)
+{
+    if(lhs.get_suit == rhs.get_suit)
+        if(lhs.get_rank == rhs.get_rank)
+            return true;
+    return false;
+}
+
+//EFFECTS Returns true if lhs is not the same card as rhs.
+//  Does not consider trump.
+bool operator!=(const Card &lhs, const Card &rhs)
+{
+    if(lhs.get_suit != rhs.get_suit)
+        if(lhs.get_rank != rhs.get_rank)
+            return true;
+    return false;
+}
+
+//REQUIRES suit is a valid suit
+//EFFECTS returns the next suit, which is the suit of the same color
+std::string Suit_next(const std::string &suit)
+{
+    if(suit == "Spades")
+        return "Clubs";
+    else if(suit == "Clubs")
+        return "Spades";
+    else if(suit == "Hearts")
+        return "Diamonds";
+    else
+        return "Hearts";
+}
