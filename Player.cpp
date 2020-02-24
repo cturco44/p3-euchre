2aedbd5a49ee5f258fb040bce5f118cb9b72a24c
mgmii <mgmii@umich.edu>
2020-02-24 14:09:43 -0500

    Added functions for human player.

--- Player.cpp
+++ Player.cpp
@@ -8,6 +8,9 @@
 #include <vector>
 #include <algorithm>
 
+
+using namespace std;
+
 class SimplePlayer : public Player {
 private:
     std::string name;
@@ -120,45 +123,134 @@ public:
 
 class HumanPlayer : public Player {
 private:
-    std::string name;
+    string name;
+    vector<Card> hand;
+    static void print_hand(vector<Card> &hand){
+        for(size_t i = 0; i < hand.size(); ++i)
+        {
+            cout << i+1 << ". " << hand.at(i) << endl;
+        }
+    }
 public:
-    HumanPlayer(std::string name_in)
+    HumanPlayer(string name_in)
     : name(name_in) {}
     //EFFECTS returns player's name
-    virtual const std::string & get_name() const {
+    const string & get_name() const{
         return name;
     }
     //REQUIRES player has less than MAX_HAND_SIZE cards
     //EFFECTS  adds Card c to Player's hand
     virtual void add_card(const Card &c) {
-        assert(false);
+        hand.push_back(c);
+        sort(hand.begin(), hand.end());
     }
     //REQUIRES round is 1 or 2
     //MODIFIES order_up_suit
     //EFFECTS If Player wishes to order up a trump suit then return true and
     //  change order_up_suit to desired suit.  If Player wishes to pass, then do
     //  not modify order_up_suit and return false.
-    virtual bool make_trump(const Card &upcard, bool is_dealer,
-                            int round, std::string &order_up_suit) const {
-        assert(false);
+    bool make_trump(const Card &upcard, bool is_dealer,
+                            int round, std::string &order_up_suit) {
+        sort(hand.begin(), hand.end());
+        print_hand(hand);
+        string userDecision;
+        if(round == 1)
+        {
+            cout<<"Enter pass or " << upcard.get_suit()<<":";
+            cin >> userDecision;
+            cout << endl;
+            if(userDecision == "pass")
+                return false;
+            else if(userDecision == upcard.get_suit())
+            {
+                order_up_suit = upcard.get_suit();
+                return true;
+            }
+            else
+            {
+                cout<<"Invalid input"<<endl;
+                return false;
+            }
+        }
+        else if(round ==2){
+            cout<<"Enter pass, Diamonds, Spades, Hearts, Clubs:";
+            cin >> userDecision;
+            cout<<endl;
+            if(userDecision == "pass")
+                return false;
+            else if(userDecision == "Diamonds")
+            {
+                order_up_suit = "Diamonds";
+                return true;
+            }
+            else if(userDecision == "Hearts")
+            {
+                order_up_suit = "Hearts";
+                return true;
+            }
+            else if(userDecision == "Spades")
+            {
+                order_up_suit = "Spades";
+                return true;
+            }
+            else if(userDecision == "Clubs")
+            {
+                order_up_suit = "Clubs";
+                return true;
+            }
+            else{cout<<"Invalid input"<<endl; return false; }
+        }
+        else{cout<<"you messed up"; return false;}
+        
     }
     //REQUIRES Player has at least one card
     //EFFECTS  Player adds one card to hand and removes one card from hand.
     virtual void add_and_discard(const Card &upcard) {
-        assert(false);
+        
+        sort(hand.begin(), hand.end());
+        string userDecision;
+        print_hand(hand);
+        cout<< "-1. Discard " << upcard <<endl;
+        cout<< "Choose:";
+        cin >> userDecision;
+        int choice = stoi(userDecision);
+        if(choice != -1)
+        {
+            hand.erase(hand.begin()+choice-1);
+            hand.push_back(upcard);
+            return;
+        }
+        else
+            return;
     }
     //REQUIRES Player has at least one card, trump is a valid suit
     //EFFECTS  Leads one Card from Player's hand according to their strategy
     //  "Lead" means to play the first Card in a trick.  The card
     //  is removed the player's hand.
     virtual Card lead_card(const std::string &trump) {
-        assert(false);
+        sort(hand.begin(), hand.end());
+        string userDecision;
+        print_hand(hand);
+        cout<<"Select a card:";
+        cin >> userDecision;
+        int choice = stoi(userDecision);
+        Card temp = hand.at(choice-1);
+        hand.erase(hand.begin()+choice-1);
+        return temp;
     }
     //REQUIRES Player has at least one card, trump is a valid suit
     //EFFECTS  Plays one Card from Player's hand according to their strategy.
     //  The card is removed from the player's hand.
     virtual Card play_card(const Card &led_card, const std::string &trump) {
-        assert(false);
+        sort(hand.begin(), hand.end());
+        string userDecision;
+        print_hand(hand);
+        cout<<"Select a card:";
+        cin >> userDecision;
+        int choice = stoi(userDecision);
+        Card temp = hand.at(choice-1);
+        hand.erase(hand.begin()+choice-1);
+        return temp;
     }
     
 };
@@ -182,5 +274,4 @@ Player * Player_factory(const std::string &name, const std::string &strategy) {
 std::ostream & operator<<(std::ostream &os, const Player &p) {
     return os << p.get_name();
 }
-
 
